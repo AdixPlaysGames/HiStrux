f6fa7758f25325e2f19dc60f5495f412232d0c6c
Wiktor Wierzchowski <w.wierzch@gmail.com>
2025-01-04 01:32:02 +0100

    selection correction

--- package/model/selection.py
+++ package/model/selection.py
@@ -8,16 +8,19 @@ import h5py
 # data selection definitions
 
 def load_cells_names(population_dir: str, 
-                     num: int) -> list[str]:
+                     cells_num: Optional[int] = None,
+                     cells_names: Optional[list[int]] = None) -> list[str]:
     """
-    Reads first n names of cells from scool file.
+    Reads required number of cell names from scool file taking first cells. If user knows the names of cells he want to work with this function can be used as a check whether those cells are actually in the file.
 
     Parameters
     ----------
     population_dir : str
-        Scool file directory
-    num : int
-        Number of cell's names to be read
+        Scool file directory.
+    num : ptional[int] = None
+        Number of cell's names to be read.
+    cells_names : Optional[list[int]] = None
+        Name of cell's names to be checked if present.
 
     Returns
     -------
@@ -28,7 +31,16 @@ def load_cells_names(population_dir: str,
     --------
     >>> load_cells_names('/data/my_population.scool', 3)
     ['Cell1', 'Cell2', 'Cell3']
+    >>> load_cells_names('/data/my_population.scool', ['Cell1', 'Cell2', 'Cell4])
+    ['Cell1', 'Cell2']
     """
+    
+    try:
+        if (cells_num is None and cells_names is None) or (cells_num is not None and cells_names is not None):
+            raise ValueError("ValueError! You must pass either 'cells_num' or 'cells_names', but not both.")
+    except ValueError as e:
+        return str(e)
+
     with h5py.File(population_dir, 'r') as f:
         print('Plik otwarty pomyślnie')
         print('Atrybuty scool:')
@@ -36,29 +48,35 @@ def load_cells_names(population_dir: str,
             print(attr, ': ', f.attrs[attr])
 
         cells_all = list(f.keys())
-        if len(cells_all) < num:
-            cells = cells_all 
+
+        if cells_num is not None:
+            if len(cells_all) < cells_num:
+                cells = cells_all 
+            else:
+                cells = cells_all[:cells_num]
         else:
-            cells = cells_all[:num]
+            cells = [cell for cell in cells_all if cell in cells_names]
 
         return cells
 
 def load_data(cell_dir: str, 
               chroms_list: list[str], 
-              normalization_percentile: Optional[int] = 90) -> tuple[np.ndarray, pd.Series]:
+              do_not_clean: Optional[bool] = False,
+              normalization_percentile: Optional[int] = 90
+              ) -> tuple[np.ndarray, pd.Series]:
     """
-    Loads a contacts matrix and a bins series of specified cell from .scool file
-
-    This function extracts scHiC contact matrix and bins series describing this matrix. It picks only specified chromosomes. 
+    Extracts scHiC contact matrix and bins series describing this matrix. Data is loaded from cool file data source. If user works with composed population data within scool file the cell selection need to follow format: scool_file_directory/scool_file_name :: cell_name. It picks only specified chromosomes. Functions remove_diag_plus and normalize_hic are included in it by default, but can be turned of with a parameter.
     
     Parameters
     ----------
     cell_dir : str
-        Scool file directory with cell name
+        Scool file directory with cell name.
     chroms_list : list of str
-        List of chromosome names to loaded 
-    normalization_percentile : int
-		Percentile of weakest contacts to be removed from contact matrices in.
+        List of chromosome names to loaded.
+    do_not_clean :  Optional[bool] = False
+        True/False value controling whether normalization and main diagonal removal takes place.
+    normalization_percentile : Optional[int] = 90
+        Percentile of weakest contacts to be removed from contact matrices in.
 
     Returns
     -------
@@ -75,18 +93,18 @@ def load_data(cell_dir: str,
         [0., 0., 0., ..., 0., 0., 0.],
         [0., 0., 0., ..., 0., 0., 0.],
         [0., 0., 0., ..., 0., 0., 0.]]),
-		chrom      start        end
-	0    chr10          0    1000000
-	1    chr10    1000000    2000000
-	2    chr10    2000000    3000000
-	3    chr10    3000000    4000000
-	4    chr10    4000000    5000000
-	..     ...        ...        ...
-	721  chr15   99000000  100000000
-	722  chr15  100000000  101000000
-	723  chr15  101000000  102000000
-	724  chr15  102000000  103000000
-	725  chr15  103000000  103494974
+        chrom      start        end
+    0    chr10          0    1000000
+    1    chr10    1000000    2000000
+    2    chr10    2000000    3000000
+    3    chr10    3000000    4000000
+    4    chr10    4000000    5000000
+    ..     ...        ...        ...
+    721  chr15   99000000  100000000
+    722  chr15  100000000  101000000
+    723  chr15  101000000  102000000
+    724  chr15  102000000  103000000
+    725  chr15  103000000  103494974
     """
     #load global cell selectors
     cell = cooler.Cooler(cell_dir)
@@ -98,9 +116,10 @@ def load_data(cell_dir: str,
         fetch_end += cell_contacts.fetch(chrom).shape[0]
     
     #fetch desired part of HiC matrix
-    hic = np.log(cell_contacts[0:fetch_end, 0:fetch_end]+1)
-    remove_diag_plus(hic)
-    normalize_hic(hic, normalization_percentile)
+    hic = cell_contacts[0:fetch_end, 0:fetch_end]
+    if do_not_clean is False:
+        remove_diag_plus(hic)
+        normalize_hic(hic, normalization_percentile)
     bins_hic = cell.bins()[:]
     bins_hic = bins_hic[bins_hic['chrom'].isin(chroms_list)]
 
@@ -108,7 +127,7 @@ def load_data(cell_dir: str,
 
 def remove_diag_plus(matrix: np.ndarray) -> None:
     """
-    Removes main diagonal of matrix, and both one above and below it.
+    Sets main diagonal of matrix, and together with diagonals one above and below it to zero. This function is used as part of preprocessing to remove bins contacts on diagonal and those of neighboring bins which are not useful in chromatin reconstruction. It is used by default in load_data function.
     
     Parameters
     ----------
@@ -123,7 +142,7 @@ def remove_diag_plus(matrix: np.ndarray) -> None:
     Examples
     --------
     >>> matrix = np.ndarray((4,4))
-	>>> matrix[:] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    >>> matrix[:] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     >>> remove_diag_plus(matrix)
     >>> print(matrix)
     array([[0, 0, 3, 4],
@@ -138,7 +157,7 @@ def remove_diag_plus(matrix: np.ndarray) -> None:
 
 def normalize_hic(hic: np.ndarray, p: int) -> None:
     """
-    Sets p-th percentile of lowest values in hic matrix to 0.
+    Applies natural logarithm over matrix values and sets p-th percentile of lowest values to 0. This function is part of data preprocessing and is used by default in load_data function.
     
     Parameters
     ----------
@@ -153,7 +172,7 @@ def normalize_hic(hic: np.ndarray, p: int) -> None:
     Examples
     --------
     >>> matrix = np.ndarray((4,4))
-	>>> matrix[:] = [[0, 0, 3, 4], [0, 0, 0, 8], [9, 0, 0, 0], [13, 14, 0, 0]]
+    >>> matrix[:] = [[0, 0, 3, 4], [0, 0, 0, 8], [9, 0, 0, 0], [13, 14, 0, 0]]
     >>> normalize_hic(matrix)
     >>> print(matrix)
     array([[0, 0, 0, 0],
@@ -164,34 +183,34 @@ def normalize_hic(hic: np.ndarray, p: int) -> None:
     flat = hic.flatten()
     thersh = np.percentile(flat, p)
     hic[hic < thersh] = 0
+    hic = np.log(hic + 1)
 
 def filter_poor_cells(population_dir: str, 
                       cells_names: list[str], 
                       chroms_list: list[str], 
                       min_contacts: Optional[int] = 4000, 
                       min_ratio: Optional[int] = 0.15, 
-                      max_ratio: Optional[int] = 0.4, main_width: Optional[int] = 50) -> np.array:
+                      max_ratio: Optional[int] = 0.4, 
+                      main_width: Optional[int] = 50) -> np.array:
     """
-    Filters provided list of cells names with respect to contacts matricies quality. 
-
-    This function filters list of cells names and returns only cells which contact matrices have required minimal number of contacts as well as minimal and maximal ratio of long range contacts. Where long range contacts are defined as one's located beyond main_width central diagonals of matrix.    
+    This function filters list of cells names and returns only cells which contact matrices have required minimal number of contacts as well as minimal and maximal ratio of long range contacts. Where long range contacts are defined as one's located beyond main\_width central diagonals of matrix.  
     
     Parameters
     ----------
     population_dir: str
-		Scool file directory containing cells data
+        Scool file directory containing cells data.
     cells_names: list[str]
-		List of cells names to be filtered
+        List of cells names to be filtered.
     chroms_list: list[str]
-        List of chromosome names to be considered
+        List of chromosome names to be considered.
     min_contacts: Optional[int] = 4000
-		Requires number of contacts to be higher than that.
+        Requires number of contacts to be higher than that.
     min_ratio: Optional[int] = 0.15
-		Requires ratio of long range contacts to be higher than that.
+        Requires ratio of long range contacts to be higher than that.
     max_ratio: Optional[int] = 0.4
-		Requires ratio of long range contacts to be lower than that.
+        Requires ratio of long range contacts to be lower than that.
     main_width: Optional[int] = 50
-		Defines long range contacts. If contacts is located on diagonal further than 50 diagonals up or down main diagonal of matrix it is constidered long range.
+        Defines long range contacts. If contacts is located on diagonal further than 50 diagonals up or down main diagonal of matrix it is constidered long range.
 
     Returns
     -------
@@ -236,24 +255,24 @@ def sample_series(cells_names: list[str],
                   series_size: int, 
                   debug=False) -> tuple[np.array, np.array, np.array]:
     """
-    Samples desired number of cells from provided cells population. Keep input ratios of labels.
+    Samples desired number of cells from provided cells population. Keep input ratios of labels. Function returns three arrays with sampled cells names, labels and prediction values, all crucial for further steps.
     
     Parameters
     ----------
     cells_names: list[str]
-		List of cells names to be filtered
+        List of cells names to be filtered.
     labels: list[int]
-		List of interphase labels given to the cells
+        List of interphase labels given to the cells.
     predictions: list[np.ndarray]
-		List of numpy ndarrays describing prediction 
+        List of numpy ndarrays describing prediction.
     series_size: int
-		Number of cells to be sampled 
+        Number of cells to be sampled.
 
     Returns
     -------
     numpy.array
         Array of names of cells sampled in a series.
-	numpy.array
+    numpy.array
         Array of labels of cells sampled in a series.
     numpy.array
         Array of prediction values of cells sampled in a series.
@@ -261,39 +280,39 @@ def sample_series(cells_names: list[str],
     Examples
     --------
     >>> print(filtered_population_names)
-	['Diploid_10_ACTGAGCG_AAGGCTAT_R1fastqgz'
-	'Diploid_10_ACTGAGCG_CCTAGAGT_R1fastqgz'
-	'Diploid_10_ACTGAGCG_CTATTAAG_R1fastqgz'
-	'Diploid_10_ACTGAGCG_GAGCCTTA_R1fastqgz'
-	'Diploid_10_ACTGAGCG_GCGTAAGA_R1fastqgz'
-	'Diploid_10_ACTGAGCG_TCGACTAG_R1fastqgz'
-	'Diploid_10_ATGCGCAG_AAGGCTAT_R1fastqgz'
-	'Diploid_10_ATGCGCAG_CCTAGAGT_R1fastqgz'
-	'Diploid_10_ATGCGCAG_CTATTAAG_R1fastqgz'
-	'Diploid_10_ATGCGCAG_GCGTAAGA_R1fastqgz']
-	>>> print(population_labels)
-	[0 2 2 2 2 0 2 0 0 1]
-	>>> print(population_predictions)
-	[[0.50630042 0.2317334  0.26196618]
-	[0.35175224 0.23726902 0.41097873]
-	[0.35609426 0.02918836 0.61471738]
-	[0.15329386 0.26029309 0.58641306]
-	[0.31415687 0.17877836 0.50706477]
-	[0.53080315 0.21029382 0.25890303]
-	[0.32800212 0.03946776 0.63253011]
-	[0.40546755 0.35522468 0.23930777]
-	[0.37774182 0.26117363 0.36108455]
-	[0.40079315 0.46998894 0.12921791]]
+    ['Diploid_10_ACTGAGCG_AAGGCTAT_R1fastqgz'
+    'Diploid_10_ACTGAGCG_CCTAGAGT_R1fastqgz'
+    'Diploid_10_ACTGAGCG_CTATTAAG_R1fastqgz'
+    'Diploid_10_ACTGAGCG_GAGCCTTA_R1fastqgz'
+    'Diploid_10_ACTGAGCG_GCGTAAGA_R1fastqgz'
+    'Diploid_10_ACTGAGCG_TCGACTAG_R1fastqgz'
+    'Diploid_10_ATGCGCAG_AAGGCTAT_R1fastqgz'
+    'Diploid_10_ATGCGCAG_CCTAGAGT_R1fastqgz'
+    'Diploid_10_ATGCGCAG_CTATTAAG_R1fastqgz'
+    'Diploid_10_ATGCGCAG_GCGTAAGA_R1fastqgz']
+    >>> print(population_labels)
+    [0 2 2 2 2 0 2 0 0 1]
+    >>> print(population_predictions)
+    [[0.50630042 0.2317334  0.26196618]
+    [0.35175224 0.23726902 0.41097873]
+    [0.35609426 0.02918836 0.61471738]
+    [0.15329386 0.26029309 0.58641306]
+    [0.31415687 0.17877836 0.50706477]
+    [0.53080315 0.21029382 0.25890303]
+    [0.32800212 0.03946776 0.63253011]
+    [0.40546755 0.35522468 0.23930777]
+    [0.37774182 0.26117363 0.36108455]
+    [0.40079315 0.46998894 0.12921791]]
     >>> sample_series(filtered_population_names, population_labels, population_predictions, 4)
     (array(['Diploid_10_ACTGAGCG_TCGACTAG_R1fastqgz',
         'Diploid_10_ATGCGCAG_GCGTAAGA_R1fastqgz',
         'Diploid_10_ACTGAGCG_GCGTAAGA_R1fastqgz',
         'Diploid_10_ACTGAGCG_GAGCCTTA_R1fastqgz'], dtype='<U38'),
-	array([0, 1, 2, 2]),
-	array([[0.53080315, 0.21029382, 0.25890303],
-			[0.40079315, 0.46998894, 0.12921791],
-			[0.31415687, 0.17877836, 0.50706477],
-			[0.15329386, 0.26029309, 0.58641306]]))
+    array([0, 1, 2, 2]),
+    array([[0.53080315, 0.21029382, 0.25890303],
+            [0.40079315, 0.46998894, 0.12921791],
+            [0.31415687, 0.17877836, 0.50706477],
+            [0.15329386, 0.26029309, 0.58641306]]))
     """
     labels_types, labels_counts = np.unique(labels, return_counts=True)
     if debug:	
